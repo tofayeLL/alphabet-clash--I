0a29c1b94ccf83e677e84c60fbae4888a29dc818
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-15 01:39:22 +0600

    finished the game event handle part

--- script/alphabet-clash.js
+++ script/alphabet-clash.js
@@ -1,17 +1,51 @@
 
+function handleKeyboardPressButton(event) {
+    const playerPressed = event.key;
+
+    // press escape key then the game will be over
+    if (playerPressed === 'Escape') {
+        gameOver();
+    }
+
+    const displayAlphabet = document.getElementById('display-alphabet');
+    const alphabet = displayAlphabet.innerText;
+    const alphabetToSmall = alphabet.toLocaleLowerCase();
 
 
+    // check condition
+    if (playerPressed === alphabetToSmall) {
+        console.log('wow you will get a point');
 
+        // update score
+        const currentScore = getTextElementValueById('score');
+        const updateScore = currentScore + 1;
+        setTextElementValueById('score', updateScore);
 
-function handleKeyboardButtonPress (){
-    console.log('button press')
+        // continue play game
+        removeBackgroundcolor(alphabetToSmall);
+        continueGame();
+    }
+    else {
+        console.log('opps!! you lose a life');
+
+        // update life
+        const currentLife = getTextElementValueById('life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('life', updateLife);
+
+        if (updateLife === 0) {
+            gameOver();
+        }
+
+    }
 }
 
-// captured keyboard keypress
-document.addEventListener('keyup', handleKeyboardButtonPress);
+
+// capture user key pressed 
+document.addEventListener('keyup', handleKeyboardPressButton);
 
 // after play function then continuegame() function 
-function continueGame(){
+function continueGame() {
     // step 1: generate a random alphabet
     const alphabet = getRandomAlphabet();
     console.log(alphabet);
@@ -26,28 +60,34 @@ function continueGame(){
 
 
 // for starting play 
-function play(){
-   hiddenClassAdded('home-screen');
-   hiddenClassRemove('play-ground');
-   continueGame();
+function play() {
+    // hide ground
+    hiddenClassAdded('home-screen');
+    hiddenClassAdded('final-score-board')
+    hiddenClassRemove('play-ground');
+
+    /* reset score */
+    setTextElementValueById('life', 5);
+    setTextElementValueById('score', 0);
+
+    continueGame();
 }
 
 
-// for get random alphabet
-function getRandomAlphabet(){
-    // create a random alphabet array
-    const alphabetString = 'makwltopciqnzdjrhcuxebgfsv';
-    const alphabets = alphabetString.split('');
-    
+function gameOver() {
 
-    // get a random index between 0 - 25;
-    const randomNumber = Math.random() * 25;
-    const randomIndex = Math.round(randomNumber);
+    hiddenClassAdded('play-ground');
+    hiddenClassRemove('final-score-board');
+    const score = getTextElementValueById('score')
+    setTextElementValueById('final-update-score', score);
 
-    const randomAlphabet = alphabets[randomIndex];
-   
-    return randomAlphabet;
-    
+    // current alphabet inner text
+    const alphabet = getElementInnerTextById('display-alphabet');
+    removeBackgroundcolor(alphabet);
+
 }
+
+
+
 
 
--- script/utility.js
+++ script/utility.js
@@ -1,20 +1,67 @@
 
 // for hidden class add and hidden class remove reusable function
-function hiddenClassAdded(elementId){
+function hiddenClassAdded(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function hiddenClassRemove(elementId){
+function hiddenClassRemove(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
 
 // set background color reusable function
-function setBackgroundcolor(elementId){
+function setBackgroundcolor(elementId) {
     const element = document.getElementById(elementId);
-    element.style.backgroundColor = '#ffa500'
+    element.classList.add('bg-[#ffa500]');
+}
+
+// remove background color
+function removeBackgroundcolor(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#ffa500]');
+
+}
+
+// score and life element get
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const elementValue = parseInt(elementValueText);
+    return elementValue;
+}
+
+
+// to set value as a inner text any element
+function setTextElementValueById(elementId, elementValue) {
+    const element = document.getElementById(elementId);
+    element.innerText = elementValue;
+}
+
+// get inner text from an element
+function getElementInnerTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const innerText = element.innerText;
+    return innerText;
+
+}
+
+
+// for get random alphabet
+function getRandomAlphabet() {
+    // create a random alphabet array
+    const alphabetString = 'makwltopciqnzdjrhcuxebgfsv';
+    const alphabets = alphabetString.split('');
+
+    // get a random index between 0 - 25;
+    const randomNumber = Math.random() * 25;
+    const randomIndex = Math.round(randomNumber);
+
+    const randomAlphabet = alphabets[randomIndex];
+
+    return randomAlphabet;
+
 }
 
 
